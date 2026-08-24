6c4e9c24f8879151b74f3f43220f6b1630db7bcb
86jaewooryu-web <86jaewoo.ryu@gmail.com>
2026-08-25 07:03:33 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,17 @@
 import type { Metadata } from "next";
-import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Portfolio Website",
+  title: "Jz Portfolio API",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ko">
-      <body style={{ backgroundColor: '#000', margin: 0, padding: 0 }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
