403d4a38d705fcfff4ac2d2f74e606bf9f95642b
86jaewooryu-web <86jaewoo.ryu@gmail.com>
2026-08-25 06:43:08 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Jz Communications",
-  description: "Portfolio",
+  title: "Portfolio",
+  description: "Portfolio Website",
 };
 
 export default function RootLayout({
